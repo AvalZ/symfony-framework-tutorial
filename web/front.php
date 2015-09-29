f6272751cc5f0f83740b98a1347f99f51697703f
Andrea Valenza <avalenza89@gmail.com>
2015-09-29 11:08:00 +0200

    Add listeners handling.

--- web/front.php
+++ web/front.php
@@ -6,24 +6,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
-function render_template($request)
-{
-	extract($request->attributes->all(), EXTR_SKIP);
-	ob_start();
-	include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+$dispatcher = new EventDispatcher();
 
-	return new Response(ob_get_clean());
-}
+$dispatcher->addListener('response', array(new Simplex\ContentLengthListener(), 'onResponse'), -255);
+$dispatcher->addListener('response', array(new Simplex\GoogleListener(), 'onResponse'));
 
-$request = Request::createFromGlobals();
-$routes = include __DIR__.'/../src/app.php';
-
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
-
-$framework = new Simplex\Framework($matcher, $resolver);
+$framework = new Simplex\Framework($dispatcher, $matcher, $resolver);
 $response = $framework->handle($request);
 
 $response->send();
